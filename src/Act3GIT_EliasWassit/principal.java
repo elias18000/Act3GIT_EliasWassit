c45786655fb70b3f52b7c66df02516cbad8365fd
Elias Wassit <ewascal004@g.educaand.es>
2024-11-20 10:04:33 +0100

    añado programa para dar numeros random del 1 al 10

---
+++ src/Act3GIT_EliasWassit/principal.java
@@ -0,0 +1,13 @@
+package Act3GIT_EliasWassit;
+import java.util.*;
+public class principal {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		for (int i = 0; i < 20; i++) {
+			int numero = rand.nextInt(10);
+			System.out.println((i + 1 ) + ": " + numero);
+		}
+	}
+
+}
