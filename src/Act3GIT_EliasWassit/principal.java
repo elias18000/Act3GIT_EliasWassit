b3bdf32ef401ea181dbc8db33f0400f3e4c74af5
Elias Wassit <ewascal004@g.educaand.es>
2024-11-23 10:39:46 +0100

    incluyo numero3

--- src/Act3GIT_EliasWassit/principal.java
+++ src/Act3GIT_EliasWassit/principal.java
@@ -13,8 +13,14 @@ public class principal {
 		System.out.println("Dame dos numeros y te dre los numeros que hay en medio contando de 7 en 7: ");
 		int numero1 = Integer.parseInt(scan.next());
 		int numero2 = Integer.parseInt(scan.next());
+		System.out.println("dame un tercer numero");
+		int numero3 = Integer.parseInt(scan.next());
+	
 		for (int i = numero1; i <= numero2; i += 7) {
 			System.out.println("numero: " + i);
+		}
+		if(numero3 >= numero1 && numero3 <= numero2 ) {
+		System.out.println("numero3: " + numero3 );
 		}
 	}
 }
