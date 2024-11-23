a8377357dd3e26b2a3ab598f902e7a21bf07a2d8
Elias Wassit <ewascal004@g.educaand.es>
2024-11-23 10:29:41 +0100

    actualización numeros de 7 en 7

--- src/Act3GIT_EliasWassit/principal.java
+++ src/Act3GIT_EliasWassit/principal.java
@@ -3,10 +3,18 @@ import java.util.*;
 public class principal {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
+		
 		for (int i = 0; i < 20; i++) {
 			int numero = rand.nextInt(10);
 			System.out.println((i + 1 ) + ": " + numero);
+		}
+		System.out.println("Dame dos numeros y te dre los numeros que hay en medio contando de 7 en 7: ");
+		int numero1 = Integer.parseInt(scan.next());
+		int numero2 = Integer.parseInt(scan.next());
+		for (int i = numero1; i <= numero2; i += 7) {
+			System.out.println("numero: " + i);
 		}
 	}
 }
